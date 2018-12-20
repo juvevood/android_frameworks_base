4436ffa7b16e91c46110d1dec8702b4f194be8c5
Alex Cruz <alex@dirtyunicorns.com>
2018-12-19 20:36:08 -0600

    Navbar: Add Volume panel action [1/2]
    Change-Id: I2c75e6043c044ec6adde7e920f1487dbff1c16df

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1089,16 +1089,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private void toggleFlashLight() {
-        performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, true);
-        Utils.toggleCameraFlash();
-    }
-
-    private void toggleClearNotifications() {
-        performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, true);
-        Utils.clearAllNotifications();
-    }
-
     private UEventObserver mHDMIObserver = new UEventObserver() {
         @Override
         public void onUEvent(UEventObserver.UEvent event) {
@@ -4174,6 +4164,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             || doubleTapBehavior == NavbarUtilities.KEY_ACTION_FLASHLIGHT
                             || longPressBehavior == NavbarUtilities.KEY_ACTION_CLEAR_NOTIFICATIONS
                             || doubleTapBehavior == NavbarUtilities.KEY_ACTION_CLEAR_NOTIFICATIONS
+                            || longPressBehavior == NavbarUtilities.KEY_ACTION_VOLUME_PANEL
+                            || doubleTapBehavior == NavbarUtilities.KEY_ACTION_VOLUME_PANEL
                             || longPressBehavior == NavbarUtilities.KEY_ACTION_LAST_APP
                             || doubleTapBehavior == NavbarUtilities.KEY_ACTION_LAST_APP) {
                         preloadRecentApps();
@@ -9859,6 +9851,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_CLEAR_NOTIFICATIONS:
                 toggleClearNotifications();
                 break;
+            case NavbarUtilities.KEY_ACTION_VOLUME_PANEL:
+                toggleVolumePanel();
+                break;
         }
     }
 
@@ -9886,5 +9881,23 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 NavbarUtilities.launchCamera();
                 break;
         }
+    }
+
+    // Flashlight
+    private void toggleFlashLight() {
+        performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, true);
+        Utils.toggleCameraFlash();
+    }
+
+    // Clear notifications
+    private void toggleClearNotifications() {
+        performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, true);
+        Utils.clearAllNotifications();
+    }
+
+    // Volume panel
+    private void toggleVolumePanel() {
+        performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, true);
+        Utils.toggleVolumePanel(mContext);
     }
 }
