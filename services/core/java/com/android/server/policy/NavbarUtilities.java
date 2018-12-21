d15160ea3931cb23e401d33edf917cad68c4da07
Alex Cruz <alex@dirtyunicorns.com>
2018-12-20 18:59:49 -0600

    Navbar: Add Screen off action [1/2]
    Change-Id: Ib3c518be31b2ea91611dfe067065ee520bc95df6

--- services/core/java/com/android/server/policy/NavbarUtilities.java
+++ services/core/java/com/android/server/policy/NavbarUtilities.java
@@ -41,6 +41,7 @@ public class NavbarUtilities {
     public static final int KEY_ACTION_CLEAR_NOTIFICATIONS = 10;
     public static final int KEY_ACTION_VOLUME_PANEL = 11;
     public static final int KEY_ACTION_KILL_APP = 12;
+    public static final int KEY_ACTION_SCREEN_OFF = 13;
 
     // Special values, used internal only.
     public static final int KEY_ACTION_HOME = 100;
@@ -125,7 +126,8 @@ public class NavbarUtilities {
             KEY_ACTION_FLASHLIGHT,
             KEY_ACTION_CLEAR_NOTIFICATIONS,
             KEY_ACTION_VOLUME_PANEL,
-            KEY_ACTION_KILL_APP
+            KEY_ACTION_KILL_APP,
+            KEY_ACTION_SCREEN_OFF,
     };
 
     /**
