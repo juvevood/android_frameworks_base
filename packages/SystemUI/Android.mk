2629a5f399612af07e5e0c4514b64a7c9ccd3dd6
Alex Cruz <du.alexcruz@gmail.com>
2019-01-16 00:30:28 -0600

    Navbar height/width: Use a custom seekbar
    This just looks a lot better and gives the user some flexibility.
    
    Change-Id: I3bd4e16d6a52298437297a3b011de8500f28f3cb

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -80,6 +80,7 @@ ifneq ($(INCREMENTAL_BUILDS),)
 endif
 
 include frameworks/base/packages/SettingsLib/common.mk
+include vendor/support/common.mk
 
 LOCAL_AAPT_FLAGS := --extra-packages com.android.keyguard
 
